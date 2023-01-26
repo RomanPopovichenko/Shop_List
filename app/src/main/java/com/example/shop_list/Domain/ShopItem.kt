8eddb001fbb9bc519757f3a1adb0879dba8a6a44
roman <aregsa2@gmail.com>
2023-01-26 20:51:20 +0200

    was added data part

--- app/src/main/java/com/example/shop_list/Domain/ShopItem.kt
+++ app/src/main/java/com/example/shop_list/Domain/ShopItem.kt
@@ -1,8 +1,12 @@
 package com.example.shop_list.Domain
 
 data class ShopItem(
-    val id: Int,
     val name: String,
     val count: Int,
-    val isActive: Boolean
-)
+    val isActive: Boolean,
+    var id: Int = UNDEFINED_ID
+) {
+    companion object {
+        const val UNDEFINED_ID = -1
+    }
+}
